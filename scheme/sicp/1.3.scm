119235b88b34a587400418757f9846486e39a80a
liangzhao wu <wuliangzhao10@yahoo.cn>
2013-04-28 19:45:36 +0800

    add big sum

--- scheme/sicp/1.3.scm
+++ scheme/sicp/1.3.scm
@@ -1,4 +1,28 @@
-(define (big-sum a b c)
+;; solution 1				
+
+;; (define (sum-big a b c)
+;;   (define (min x y)
+;;     (if (< x y)
+;; 	x
+;; 	y))
+;;   (define (squre x)
+;;     (* x x))
+;;   (- (+ (squre a)
+;;   	(squre b)
+;;   	(squre c))
+;;      (squre (min c (min a b)))))
+
+;; solution 2
+
+(define (sum-big a b c)
+  (define (max a b)
+    (if (> a b)
+	a
+	b))
   (define (squre x)
     (* x x))
+  (+ (squre (max a b))
+     (squre (max a c))))
+    
+
   
