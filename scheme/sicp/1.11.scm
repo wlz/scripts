99b45009149d5200ada403dabf292c2bfd24491b
Liangzhao Wu <wuliangzhao10@yahoo.cn>
2013-05-02 10:23:39 +0800

    modify iter-count

--- scheme/sicp/1.11.scm
+++ scheme/sicp/1.11.scm
@@ -2,13 +2,16 @@
   (if (< n 3)
       n
       (+ (rec (- n 1))
-	 (rec (- n 2))
-	 (rec (- n 3)))))
+	 (* 2 (rec (- n 2)))
+	 (* 3 (rec (- n 3))))))
 
 (define (iter n)
   (define (iter-count a b c count)
     (if (= count 0)
 	c
-	(iter-count (+ a b c) a b (- count 1))))
+	(iter-count (+ a (* 2 b) (* 3 c))
+		    a
+		    b
+		    (- count 1))))
   (iter-count 2 1 0 n))
 	      
