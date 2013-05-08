24160d17db95fda036cfc8b2fe11adddd5544871
liangzhao wu <wuliangzhao10@yahoo.cn>
2013-05-08 18:33:03 +0800

    add 3.a

--- scheme/sicp/3a.scm
+++ scheme/sicp/3a.scm
@@ -1,1 +1,16 @@
-(define 1-to-4 (list 1 2 3 4))
+(define make-segment cons)
+
+(define seg-start car)
+
+(define seg-end cdr)
+
+(define make-vector cons)
+
+(cons 1 (cons 2 (cons 3 (cons 4 '()))))
+
+(define one-to-four (list 1 2 3 4))
+
+;; (define (scale-list s l)
+;;   (if (null? l)
+;;       nil
+;;       (cons
