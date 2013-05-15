176e49440426f1718a00d79fd988e8e2a120300a
Liangzhao Wu <wuliangzhao10@yahoo.cn>
2013-05-15 08:54:15 +0800

    edit 2.27.scm

--- scheme/sicp/2.27.scm
+++ scheme/sicp/2.27.scm
@@ -1,3 +1,10 @@
+;; (define (reverse items)
+;;   (define (iter lst result)
+;;     (if (null? lst)
+;; 	result
+;; 	(iter (cdr lst) (cons (car lst) result))))
+;;   (iter items '()))
+
 (define (reverse items)
   (define (iter lst result)
     (if (null? lst)
@@ -5,4 +12,4 @@
 	(iter (cdr lst) (cons (car lst) result))))
   (iter items '()))
 
-	  
+
