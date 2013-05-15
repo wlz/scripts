178dc6e0411a7502fab921bac5455c741e5df609
Liangzhao Wu <wuliangzhao10@yahoo.cn>
2013-05-15 17:12:39 +0800

    modify 2.27

--- scheme/sicp/2.27.scm
+++ scheme/sicp/2.27.scm
@@ -1,10 +1,3 @@
-;; (define (reverse items)
-;;   (define (iter lst result)
-;;     (if (null? lst)
-;; 	result
-;; 	(iter (cdr lst) (cons (car lst) result))))
-;;   (iter items '()))
-
 (define (reverse items)
   (define (iter lst result)
     (if (null? lst)
@@ -12,4 +5,10 @@
 	(iter (cdr lst) (cons (car lst) result))))
   (iter items '()))
 
+;; (define (deep-reverse tree)
+;;   (cond ((null? tree) '())
+;; 	((not (pair? tree)) tree)
+;; 	(else (reverse (list (deep-reverse (car tree))
+;; 			     (deep-reverse (cadr tree)))))))
 
+(define (deep-reverse
