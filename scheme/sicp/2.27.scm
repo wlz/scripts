6528961e276ee2dd525a7591118a66574d737244
liangzhao wu <wuliangzhao10@yahoo.cn>
2013-05-23 20:08:05 +0800

    modify 2.27

--- scheme/sicp/2.27.scm
+++ scheme/sicp/2.27.scm
@@ -1,2 +1,13 @@
-(define (reverse items)
-  (
+(define (deep-reverse items)
+  (define (iter lst result)
+    (if (null? lst)
+	result
+	(let ((first (car lst)))
+	  (iter (cdr lst)
+		(cons (if (pair? first)
+			   (deep-reverse first)
+			   first)
+		      result)))))
+  (iter items '()))
+
+
