9168e35f158257e45407f9d9dbc965c6f978b51f
liangzhao wu <wuliangzhao10@yahoo.cn>
2013-05-27 21:20:58 +0800

    add 2.30.scm

---
+++ scheme/sicp/2.30.scm
@@ -0,0 +1,11 @@
+(define (square-tree tree)
+  (cond ((null? tree) '())
+	((not (pair? tree))
+	 (square tree))
+	(else 
+	  (cons (square-tree (car tree))
+		(square-tree (cdr tree))))))
+
+(define (square x)
+  (* x x))
+
