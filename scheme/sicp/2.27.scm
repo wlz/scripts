ff1ce8e827662fa8d477568fbd8886cd6891b193
Liangzhao Wu <wuliangzhao10@yahoo.cn>
2013-05-28 08:52:30 +0800

    modify 2.27.scm modify hbn.txt

--- scheme/sicp/2.27.scm
+++ scheme/sicp/2.27.scm
@@ -10,3 +10,16 @@
 		      result)))))
   (iter items '()))
 
+(define (deep-rev tree)
+  (define (iter lst result)
+    (if (null? lst)
+      result
+      (let ((first (car lst)))
+	(iter (cdr lst)
+	      (cons (if (pair? first)
+		      (deep-rev first)
+		      first)
+		    result)))))
+  (iter tree '()))
+
+
