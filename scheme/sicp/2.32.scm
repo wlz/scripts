367be83db873211598fbc9d5ff9d833f95e89ffb
Liangzhao Wu <wuliangzhao10@yahoo.cn>
2013-06-07 08:36:46 +0800

    modify hbn.txt

--- scheme/sicp/2.32.scm
+++ scheme/sicp/2.32.scm
@@ -1,6 +1,4 @@
-(define (append src app)
-  (define (iter lst result)
-    (if (null? lst)
-      result
-      (iter (cdr lst) (cons result (car lst)))))
-  (iter app src))
+(define nil '())
+
+(define (subsets s)
+  (
